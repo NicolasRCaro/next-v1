39f22913c6104e49fc8d6cd4641e7b0a585ccc9a
NicolasRCaro <nicolas.ramirezc@uniagustiniana.edu.co>
2026-05-08 19:03:39 +0000

    Primer commit con copilot

--- app/dos/hijo.tsx
+++ app/dos/hijo.tsx
@@ -1,7 +1,13 @@
-export default function Hijo({children}){
+import React from 'react';
+
+interface Props {
+    children: React.ReactNode;
+}
+
+export default function Hijo({children}: Props){
     return(
         <div>
-            Ese es mi hijo {children} estoy orgulloso de el.
+            Ese es mi hijo {children} estoy orgulloso de él.
         </div>
     )
 }
