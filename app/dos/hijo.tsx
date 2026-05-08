d39cff62b494a53f2cc81ca1ecb25347b01c0f35
NicolasRCaro <nicolas.ramirezc@uniagustiniana.edu.co>
2026-05-08 19:15:23 +0000

    Primer commit auto deploy

--- app/dos/hijo.tsx
+++ app/dos/hijo.tsx
@@ -7,7 +7,7 @@ interface Props {
 export default function Hijo({children}: Props){
     return(
         <div>
-            Ese es mi hijo {children} estoy orgulloso de él.
+            Ese es mi hijo {children} no estoy orgulloso de él.
         </div>
     )
 }
